c58b8e47562b2c6b344fca55468dc3dccc67e232
Ayiks <cliffysarp@gmail.com>
2026-08-10 15:10:12 +0000

    chore(release): advance mobile version to 1.4.5

--- apps/client/test/core/constants/app_version_test.dart
+++ apps/client/test/core/constants/app_version_test.dart
@@ -3,7 +3,7 @@ import 'package:myshop_client/src/core/constants/app_version.dart';
 
 void main() {
   test('release UI defaults to the approved marketing version', () {
-    expect(appMarketingVersion, '1.4.4');
-    expect(appVersionLabel, 'Version 1.4.4');
+    expect(appMarketingVersion, '1.4.5');
+    expect(appVersionLabel, 'Version 1.4.5');
   });
 }
